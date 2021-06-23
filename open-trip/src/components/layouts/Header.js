fd1662bb57285b01f04fe1893534df34ebcd4b7a
Jadhuk Herlambang <jadhuk11@gmail.com>
2021-06-23 14:27:00 +0700

    initial commit

---
+++ open-trip/src/components/layouts/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "../styles/Header.scss";
+
+
+const Header = () => (
+    <div className="Header">
+        <div className="header__brand">
+            <div className="header__brand__container">
+                <h4>Beranda</h4>
+                <h4>Destinasi</h4>
+            </div>
+        </div>
+    </div>
+)
+ export default Header;
---
+++ open-trip/src/components/pages/HomePages.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "../layouts/Header";
+
+const HomePage = () =>{
+    return (
+        <div>
+            <Header />
+            {/* <Search />
+            <Main />
+            <Footer /> */}
+        </div>
+    )
+}
+
+export default HomePage;
