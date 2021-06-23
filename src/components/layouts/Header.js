eb627b6e127e1d9a74d2d80b7681c5cdde8065ea
Jadhuk Herlambang <jadhuk11@gmail.com>
2021-06-24 00:10:37 +0700

    inital commit header

--- src/components/layouts/Header.js
+++ src/components/layouts/Header.js
@@ -1,11 +1,17 @@
 import React from "react";
 import Login from "./Login";
 import RegisPage from "./RegisPage";
-
+import styles from "../styles/header.module.css";
 const Header = () => (
-  <div>
-    <Login />
-    <RegisPage />
+  <div className={styles.header}>
+    <div className={styles.header__brand}>
+      <div className={styles.header__container}>
+        <h4>Beranda</h4>
+        <h4>Destinasi</h4>
+        {/* <Login />
+        <RegisPage /> */}
+      </div>
+    </div>
   </div>
 );
 export default Header;
