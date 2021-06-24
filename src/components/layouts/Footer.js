57eee61a6124b8ba003f07379b568ad11a167390
Vega <hello.vegaveronica@gmail.com>
2021-06-24 09:18:01 +0700

    create footer

--- src/components/layouts/Footer.js
+++ src/components/layouts/Footer.js
@@ -1,4 +1,45 @@
 import React from "react";
+import { MDBCol, MDBContainer, MDBRow, MDBFooter } from "mdbreact";
+import logo from "../img/logo.png";
+import GetTheApp from "../layouts/GetTheApp";
+import "../styles/Footer.css";
 
-const Footer = () => <div></div>;
+const Footer = () => {
+  return (
+    <MDBFooter color="blue" className="font-small pt-4 mt-4">
+      <MDBContainer fluid className="text-center text-md-left">
+        <MDBRow>
+          <MDBCol md="6">
+            <div className="Footer">
+              <img className="logo" src={logo} alt="Logo2" />
+            </div>
+          </MDBCol>
+          <MDBCol className="footer" md="6">
+            <ul className="company">
+              <a href="#!">Company</a>
+            </ul>
+            <ul className="about">
+              <a href="#!">About</a>
+            </ul>
+            <ul className="press">
+              <a href="#!">Press</a>
+            </ul>
+            <ul className="careers">
+              <a href="#!">Careers</a>
+            </ul>
+            <ul className="contact">
+              <a href="#!">Contact</a>
+            </ul>
+          </MDBCol>
+          <MDBCol md="6">
+            <h4 className="download">Get the App</h4>
+            <GetTheApp />
+          </MDBCol>
+        </MDBRow>
+      </MDBContainer>
+      <div class="copyright">©2020 OPENTRIP. Allrights Reserved</div>
+    </MDBFooter>
+  );
+};
+
 export default Footer;
