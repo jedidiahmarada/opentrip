7535b60ee541e86dc1c7b07e81c765ec491e0f99
Jedidiah Judah Marada <jedidiahjudahmarada@hotmail.com>
2021-06-26 01:11:23 +0700

    push hasil edit banner dan footer

--- src/components/layouts/BannerB.js
+++ src/components/layouts/BannerB.js
@@ -1,6 +1,4 @@
 import React from "react";
-import logoplaystorebannerb from "../assets/picture/playstore.png";
-import logoappstorebannerb from "../assets/picture/applestore.png";
 import iphonemockup1 from "../assets/picture/iphone1mockup.png";
 import iphonemockup2 from "../assets/picture/iphone2mockup.png";
 import "../styles/bannerb.css";
@@ -8,27 +6,27 @@ import "../styles/bannerb.css";
 const BannerB = () => {
   return (
     <div className="bannerb">
-      <h6 className="descriptionh6">Download App</h6>
-      <p className="descriptionh4">
-        Download the app to manage your projects, keep track of the progress and
-        complete tasks without procastinating. Stay on track and complete on
-        time!
-      </p>
-      <ul className="gettheappbanner">
-        <p>Get The App</p>
-      </ul>
-      <img
-        className="logoplaystorebannerb"
-        src={logoplaystorebannerb}
-        alt="logoplaystorebannerb"
-      />
-      <img
-        className="logoappstorebannerb"
-        src={logoappstorebannerb}
-        alt="logoappstorebannerb"
-      />
-      <img className="iphonemockup1" src={iphonemockup1} alt="iphonemockup1" />
-      <img className="iphonemockup2" src={iphonemockup2} alt="iphonemockup2" />
+      <div className='headerB-kiri'>
+        <h1 className="descriptionh6">Download App</h1>
+        <p className="descriptionh4">
+          Download the app to manage your projects, keep track of the progress and
+          complete tasks without procastinating. Stay on track and complete on
+          time!
+        </p>
+        <h3 className='getApp'>Get The App</h3>
+        <div className='logo-appstore-headerb'>
+          <img
+            className="logoappstorebannerb"
+            src='https://pbs.twimg.com/media/E3yy9oxVgAIeyRQ?format=png&name=orig'
+          />
+          <img
+            className="logoappstorebannerb"
+            src='https://pbs.twimg.com/media/E3yy9ovVgAIaWUz?format=png&name=orig'
+          />
+        </div>
+      </div>
+      <img className="iphonemockup1" src={iphonemockup1} alt="iphonemockup1"/>
+      <img className="iphonemockup2" src={iphonemockup2} alt="iphonemockup2"/>
     </div>
   );
 };
