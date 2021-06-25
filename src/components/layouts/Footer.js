00dadd2788a2fae8462ac417216566d79a34c9a4
Jadhuk Herlambang <jadhuk11@gmail.com>
2021-06-25 09:58:13 +0700

    edit footer

--- src/components/layouts/Footer.js
+++ src/components/layouts/Footer.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { MDBCol, MDBContainer, MDBRow, MDBFooter } from "mdbreact";
 import logo from "../assets/picture/logo.png";
 import logoplaystore from "../assets/picture/playstore.png";
 import logoappstore from "../assets/picture/applestore.png";
@@ -10,75 +9,58 @@ import "../styles/Footer.css";
 
 const Footer = () => {
   return (
-    <MDBFooter color="blue" className="font-small pt-4 mt-4">
-      <MDBContainer fluid className="text-center text-md-left">
-        <MDBRow>
-          <MDBCol md="6">
-            <div className="Footer">
-              <img className="logo" src={logo} alt="Logo2" />
-            </div>
-          </MDBCol>
-          <MDBCol className="footer" md="6">
-            <ul className="company">
-              <p>Company</p>
-            </ul>
-            <ul className="about">
-              <p>About</p>
-            </ul>
-            <ul className="press">
-              <p>Press</p>
-            </ul>
-            <ul className="careers">
-              <p>Careers</p>
-            </ul>
-            <ul className="contact">
-              <p>Contact</p>
-            </ul>
-            <ul className="support">
-              <p>Support</p>
-            </ul>
-            <ul className="helpcenter">
-              <p>Help Center</p>
-            </ul>
-            <ul className="toc">
-              <p>Term of service</p>
-            </ul>
-            <ul className="legal">
-              <p>Legal</p>
-            </ul>
-            <ul className="privacypolicy">
-              <p>Privacy Policy</p>
-            </ul>
-            <ul className="kerjasama">
-              <p>Kerja Sama</p>
-            </ul>
-            <ul className="daftarjadihoster">
-              <p>Daftar Jadi Hoster</p>
-            </ul>
-            <ul className="hosterterdaftar">
-              <p>Hoster Terdaftar</p>
-            </ul>
-            <ul className="gettheapp">
-              <p>Get The App</p>
-            </ul>
-            <img
-              className="logoplaystore"
-              src={logoplaystore}
-              alt="logoplaystore"
-            />
-            <img
-              className="logoappstore"
-              src={logoappstore}
-              alt="logoapplestore"
-            />
-          </MDBCol>
-        </MDBRow>
-      </MDBContainer>
+    <div className="Footer">
+      <img className="logo" src={logo} alt="Logo2" />
+
+      <ul className="company">
+        <p>Company</p>
+      </ul>
+      <ul className="about">
+        <p>About</p>
+      </ul>
+      <ul className="press">
+        <p>Press</p>
+      </ul>
+      <ul className="careers">
+        <p>Careers</p>
+      </ul>
+      <ul className="contact">
+        <p>Contact</p>
+      </ul>
+      <ul className="support">
+        <p>Support</p>
+      </ul>
+      <ul className="helpcenter">
+        <p>Help Center</p>
+      </ul>
+      <ul className="toc">
+        <p>Term of service</p>
+      </ul>
+      <ul className="legal">
+        <p>Legal</p>
+      </ul>
+      <ul className="privacypolicy">
+        <p>Privacy Policy</p>
+      </ul>
+      <ul className="kerjasama">
+        <p>Kerja Sama</p>
+      </ul>
+      <ul className="daftarjadihoster">
+        <p>Daftar Jadi Hoster</p>
+      </ul>
+      <ul className="hosterterdaftar">
+        <p>Hoster Terdaftar</p>
+      </ul>
+      <ul className="gettheapp">
+        <p>Get The App</p>
+      </ul>
+      <img className="logoplaystore" src={logoplaystore} alt="logoplaystore" />
+      <img className="logoappstore" src={logoappstore} alt="logoapplestore" />
       <div className="copyright">©2020 OPENTRIP. All rights Reserved</div>
       <img className="instagram" src={instagram} alt="instagram" />
       <img className="twitter" src={twitter} alt="twitter" />
       <img className="youtube" src={youtube} alt="youtube" />
-    </MDBFooter>
+    </div>
   );
 };
 
--- src/components/layouts/Search.js
+++ src/components/layouts/Search.js
@@ -4,23 +4,25 @@ import { DatePickerComponent } from "@syncfusion/ej2-react-calendars";
 
 const Search = () => (
   <div className={styles.search}>
-    <div className={styles.bannerA__text}>
-      <h1>NEVER STOP</h1>
-      <h1>EXPLORING THE WORLD</h1>
-    </div>
-    <div className={styles.search__card}>
-      <div className={styles.search__destination}>
-        <input
-          className={styles.search__input}
-          type="text"
-          placeholder="Anywhere"
-        ></input>
+    <div className={styles.search__background}>
+      <div className={styles.bannerA__text}>
+        <h1>NEVER STOP</h1>
+        <h1>EXPLORING THE WORLD</h1>
       </div>
-      <div>
-        <DatePickerComponent></DatePickerComponent>
-      </div>
-      <div>
-        <button className={styles.search__btn}>Search</button>
+      <div className={styles.search__card}>
+        <div className={styles.search__destination}>
+          <input
+            className={styles.search__input}
+            type="text"
+            placeholder="Anywhere"
+          ></input>
+        </div>
+        <div>
+          <DatePickerComponent></DatePickerComponent>
+        </div>
+        <div>
+          <button className={styles.search__btn}>Search</button>
+        </div>
       </div>
     </div>
   </div>
