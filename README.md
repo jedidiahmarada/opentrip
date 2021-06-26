2a213b378b906885d47a0a8686373ba562024a9f
Jedidiah Judah Marada <jedidiahjudahmarada@hotmail.com>
2021-06-27 03:18:17 +0700

    styling login page, not done

--- README.md
+++
@@ -1,2 +0,0 @@
-# FrontendTeam_D
-
--- src/components/layouts/Login.js
+++ src/components/layouts/Login.js
@@ -1,8 +1,8 @@
 import React from "react";
-import { useState } from "react";
-import { useDispatch } from "react-redux";
+import {useState} from "react";
+import {useDispatch} from "react-redux";
 import styles from "../styles/login.module.css";
-import { loginAction } from "../redux/actions/loginAction";
+import {loginAction} from "../redux/actions/loginAction";
 
 const Login = () => {
   const [username, setUsername] = useState("");
@@ -16,14 +16,17 @@ const Login = () => {
 
   return (
     <div className={styles.box}>
-      <div className={styles.loginbg}></div>
+      <img className='left-bg' src='https://pbs.twimg.com/media/E41VcZgVgAAtZ5s?format=jpg&name=orig'/>
+      {/*<div className={styles.loginbg}></div>*/}
       <div className={styles.login__card}>
+        <p>Username</p>
         <input
           className={styles.login__input}
           placeholder="username"
           value={username}
           onChange={(e) => setUsername(e.target.value)}
         ></input>
+        <p>Password</p>
         <input
           className={styles.pswrd__input}
           placeholder="password"
