21db7d191c67c2822564254252c52abee9d782f2
Jadhuk Herlambang <jadhuk11@gmail.com>
2021-06-30 23:22:31 +0700

    UI Order Confirm. belum route

--- src/components/pages/HomePages.js
+++ src/components/pages/HomePages.js
@@ -3,14 +3,14 @@ import React from "react";
 import Search from "../layouts/Search";
 import Category from "../layouts/Category";
 import BannerB from "../layouts/BannerB";
-import OrderConfirm from "../layouts/OrderConfirm";
+// import OrderConfirm from "../layouts/OrderConfirm";
 
 const HomePage = () => (
   <div>
-    {/* <Search />
+    <Search />
     <Category />
-    <BannerB /> */}
-    <OrderConfirm />
+    <BannerB />
+    {/* <OrderConfirm /> */}
   </div>
 );
 export default HomePage;
