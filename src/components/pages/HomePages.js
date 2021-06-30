7968a7a330da39988a86d7759c261c9ee0e1f238
Jadhuk Herlambang <jadhuk11@gmail.com>
2021-06-30 23:21:23 +0700

    UI Order Confirm

--- src/components/pages/HomePages.js
+++ src/components/pages/HomePages.js
@@ -2,14 +2,15 @@ import React from "react";
 
 import Search from "../layouts/Search";
 import Category from "../layouts/Category";
-
 import BannerB from "../layouts/BannerB";
+import OrderConfirm from "../layouts/OrderConfirm";
 
 const HomePage = () => (
   <div>
-    <Search />
+    {/* <Search />
     <Category />
-    <BannerB />
+    <BannerB /> */}
+    <OrderConfirm />
   </div>
 );
 export default HomePage;
