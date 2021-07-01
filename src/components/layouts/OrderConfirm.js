364df2d51f8a55fbaf27e7dc7f6169fc2f6d7060
Jadhuk Herlambang <jadhuk11@gmail.com>
2021-07-01 10:14:04 +0700

    Add order confirm

--- src/components/layouts/OrderConfirm.js
+++ src/components/layouts/OrderConfirm.js
@@ -1,11 +1,13 @@
 import React from "react";
 import styles from "../styles/orderConfirm.module.css";
-import { Table, Button } from "react-bootstrap";
+import { Table, Alert } from "react-bootstrap";
 
 const OrderConfirm = () => {
   return (
-    <div>
-      {" "}
+    <div className={styles.orderConfirm}>
+      <h2 className={styles.table__title}>
+        Thank you. Your order has been received.
+      </h2>
       <div>
         <Table responsive="sm" hover>
           <thead>
@@ -25,7 +27,9 @@ const OrderConfirm = () => {
               <td>Table cell</td>
               <td>Table cell</td>
               <td>Table cell</td>
-              <td>Table cell</td>
+              <td>
+                <p className={styles.alert__succes}>Succes</p>
+              </td>
             </tr>
           </tbody>
         </Table>
--- src/components/pages/HomePages.js
+++ src/components/pages/HomePages.js
@@ -4,13 +4,15 @@ import Search from "../layouts/Search";
 import Category from "../layouts/Category";
 import BannerB from "../layouts/BannerB";
 import OrderReceived from "../layouts/OrderReceived";
+import OrderConfirm from "../layouts/OrderConfirm";
 
 const HomePage = () => (
   <div>
     {/* <Search />
     <Category />
-    <BannerB /> */}
-    <OrderReceived />
+    <BannerB />
+    <OrderReceived /> */}
+    <OrderConfirm />
   </div>
 );
 export default HomePage;
