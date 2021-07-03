dfe02c723f74670194c2b4bafb30c1f58be86fbf
Jedidiah Judah Marada <jedidiahjudahmarada@hotmail.com>
2021-07-03 13:38:40 +0700

    create my trip tinggal peta + benerin header & footer

--- src/components/pages/HomePages.js
+++ src/components/pages/HomePages.js
@@ -1,15 +1,23 @@
 import React from "react";
+import styles from '../styles/Homepage.module.css'
 
 import Search from "../layouts/Search";
 import Category from "../layouts/Category";
 
 import BannerB from "../layouts/BannerB";
+import CreateMyTrip from "../layouts/CreateMyTrip";
+import HosterNav from "../layouts/HosterNav";
+
 
 const HomePage = () => (
   <div>
-    <Search />
-    <Category />
-    <BannerB />
+    {/*<Search />*/}
+    {/*<Category />*/}
+    {/*<BannerB />*/}
+    <div className={styles.profileHoster}>
+      <HosterNav/>
+      <CreateMyTrip/>
+    </div>
   </div>
 );
 
