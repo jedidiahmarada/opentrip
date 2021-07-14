54dd49c4d20f9419fc1b1f0f1dda6c8f8131325f
Jadhuk Herlambang <jadhuk11@gmail.com>
2021-07-14 21:09:35 +0700

    fixing dikit

--- src/components/layouts/registers/RegisHoster.js
+++ src/components/layouts/registers/RegisHoster.js
@@ -111,164 +111,171 @@ const RegisHoster = () => {
     history.push("/phorder");
   };
   return (
-    <div className="regishosterbackground">
-      <div className="regishosterpage">
-        <br />
-        <div className="rectangle">
+    <>
+      <Header />
+      <div className="regishosterbackground">
+        <div className="regishosterpage">
           <br />
-          <div class="rectangle">
+          <div>
             <br />
+            <div class="rectangle">
+              <br />
 
-            <div>
-              <h4 className="h4">Register as Hoster</h4>
-              <div className="username">
-                <label>User Name</label>
-                <input
-                  className="usernameinput"
-                  value={usernameSet}
-                  onChange={(e) => setUsernameSet(e.target.value)}
-                />
-              </div>
-
-              <div className="password">
-                <label>Password</label>
-                <input
-                  type="password"
-                  className="passwordinput"
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                />
-              </div>
-
-              <div className="email">
-                <label>Email</label>
-                <input
-                  type="email"
-                  className="emailinput"
-                  value={emailSet}
-                  onChange={(e) => setEmailSet(e.target.value)}
-                />
-              </div>
-
-              <div className="NomorHandphone">
-                <label>Nomor Handphone</label>
-                <input
-                  type="number"
-                  className="NHinput"
-                  value={phoneSet}
-                  onChange={(e) => setPhoneSet(e.target.value)}
-                />
-              </div>
-
-              <div className="alamat">
-                <label>Alamat</label>
-                <input
-                  type="text"
-                  className="alamatinput"
-                  value={addressSet}
-                  onChange={(e) => setAddressSet(e.target.value)}
-                />
-              </div>
-
-              {/* UPLOAD KTP */}
-              <div className="uploadfotoKTP">
-                <label>Upload Foto KTP</label>
-                <input
-                  type="file"
-                  className="uploadfotoKTPinput"
-                  alt="uploadfotoKTPinput"
-                  accept="image/*"
-                  onChange={onSelectFileId}
-                />
-                <img
-                  className="uploadicon"
-                  src={identitypicSet}
-                  alt="uploadicon"
-                />
-                <h4 className="h4uploadfotoktp">Upload Foto KTP (JPG,PNG)</h4>
-                {/* <button type="submit" className="browsefileuploadKTP">
-                  Browse File
-                </button> */}
-              </div>
-
-              {/* UPLOAD FOTO SELFIE */}
-              <div className="uploadfotoselfie">
-                <label>Upload Foto Selfie Dengan KTP</label>
-                <input
-                  type="file"
-                  className="uploadfotoselfieinput"
-                  alt="uploadfotoselfieinput"
-                  accept="image/*"
-                  onChange={onSelectFileSid}
-                />
-                <img className="uploadicon2" src={sidPicSet} alt="uploadicon" />
-                <h4 className="h4uploadfotoselfie">
-                  Upload Foto Selfie Dengan KTP (JPG,PNG)
-                </h4>
-                {/* <button type="submit" className="browsefileuploadselfie">
-                  Browse File
-                </button> */}
-              </div>
-
-              <div className="bankpembayaran">
-                <div className="labelbank">
-                  <label>Bank Pembayaran</label>
+              <div>
+                <h4 className="h4">Register as Hoster</h4>
+                <div className="username">
+                  <label>User Name</label>
+                  <input
+                    className="usernameinput"
+                    value={usernameSet}
+                    onChange={(e) => setUsernameSet(e.target.value)}
+                  />
                 </div>
-                <div class="form-group col-md-4">
-                  <select name="wgtmsr" id="wgtmsr">
-                    <option value="btn">BTN</option>
-                    <option value="btpn">BTPN</option>
-                    <option value="btnsyah">BTN Syariah</option>
-                    <option value="btpnsyah">BTPN Syariah</option>
-                  </select>
+
+                <div className="password">
+                  <label>Password</label>
+                  <input
+                    type="password"
+                    className="passwordinput"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
                 </div>
-              </div>
 
-              <div className="NomorRekening">
-                <label>Nomor Rekening</label>
-                <input
-                  type="text"
-                  className="NoRekinput"
-                  value={accountnumSet}
-                  onChange={(e) => setAccountnumSet(e.target.value)}
-                />
-              </div>
+                <div className="email">
+                  <label>Email</label>
+                  <input
+                    type="email"
+                    className="emailinput"
+                    value={emailSet}
+                    onChange={(e) => setEmailSet(e.target.value)}
+                  />
+                </div>
 
-              {/* UPLOAD FOTO BUKU REKENING */}
-              <div className="fotobukutabungan">
-                <label>Foto Buku Tabungan</label>
-                <input
-                  type="file"
-                  className="fotobukutabunganinput"
-                  alt="fotobukutabunganinput"
-                  accept="image/*"
-                  onChange={onSelectFileBa}
-                />
-                <img
-                  className="uploadicon3"
-                  src={bookAccountpicSet}
-                  alt="uploadicon"
-                />
-                <h4 className="h4fotobukutabungan">
-                  Upload Foto Buku Tabungan (JPG,PNG)
-                </h4>
-                {/* <button type="submit" className="browsefileuploadKTP">
+                <div className="NomorHandphone">
+                  <label>Nomor Handphone</label>
+                  <input
+                    type="number"
+                    className="NHinput"
+                    value={phoneSet}
+                    onChange={(e) => setPhoneSet(e.target.value)}
+                  />
+                </div>
+
+                <div className="alamat">
+                  <label>Alamat</label>
+                  <input
+                    type="text"
+                    className="alamatinput"
+                    value={addressSet}
+                    onChange={(e) => setAddressSet(e.target.value)}
+                  />
+                </div>
+
+                {/* UPLOAD KTP */}
+                <div className="uploadfotoKTP">
+                  <label>Upload Foto KTP</label>
+                  <input
+                    type="file"
+                    className="uploadfotoKTPinput"
+                    alt="uploadfotoKTPinput"
+                    accept="image/*"
+                    onChange={onSelectFileId}
+                  />
+                  <img
+                    className="uploadicon"
+                    src={identitypicSet}
+                    alt="uploadicon"
+                  />
+                  <h4 className="h4uploadfotoktp">Upload Foto KTP (JPG,PNG)</h4>
+                  {/* <button type="submit" className="browsefileuploadKTP">
                   Browse File
                 </button> */}
+                </div>
+
+                {/* UPLOAD FOTO SELFIE */}
+                <div className="uploadfotoselfie">
+                  <label>Upload Foto Selfie Dengan KTP</label>
+                  <input
+                    type="file"
+                    className="uploadfotoselfieinput"
+                    alt="uploadfotoselfieinput"
+                    accept="image/*"
+                    onChange={onSelectFileSid}
+                  />
+                  <img
+                    className="uploadicon2"
+                    src={sidPicSet}
+                    alt="uploadicon"
+                  />
+                  <h4 className="h4uploadfotoselfie">
+                    Upload Foto Selfie Dengan KTP (JPG,PNG)
+                  </h4>
+                  {/* <button type="submit" className="browsefileuploadselfie">
+                  Browse File
+                </button> */}
+                </div>
+
+                <div className="bankpembayaran">
+                  <div className="labelbank">
+                    <label>Bank Pembayaran</label>
+                  </div>
+                  <div class="form-group col-md-4">
+                    <select name="wgtmsr" id="wgtmsr">
+                      <option value="btn">BTN</option>
+                      <option value="btpn">BTPN</option>
+                      <option value="btnsyah">BTN Syariah</option>
+                      <option value="btpnsyah">BTPN Syariah</option>
+                    </select>
+                  </div>
+                </div>
+
+                <div className="NomorRekening">
+                  <label>Nomor Rekening</label>
+                  <input
+                    type="text"
+                    className="NoRekinput"
+                    value={accountnumSet}
+                    onChange={(e) => setAccountnumSet(e.target.value)}
+                  />
+                </div>
+
+                {/* UPLOAD FOTO BUKU REKENING */}
+                <div className="fotobukutabungan">
+                  <label>Foto Buku Tabungan</label>
+                  <input
+                    type="file"
+                    className="fotobukutabunganinput"
+                    alt="fotobukutabunganinput"
+                    accept="image/*"
+                    onChange={onSelectFileBa}
+                  />
+                  <img
+                    className="uploadicon3"
+                    src={bookAccountpicSet}
+                    alt="uploadicon"
+                  />
+                  <h4 className="h4fotobukutabungan">
+                    Upload Foto Buku Tabungan (JPG,PNG)
+                  </h4>
+                  {/* <button type="submit" className="browsefileuploadKTP">
+                  Browse File
+                </button> */}
+                </div>
+                {loading && <div>loading...</div>}
+                {error && <div>{errorMessage}</div>}
+                {!loading && (
+                  <button className="submitregister" onClick={handleClick}>
+                    Register
+                  </button>
+                )}
               </div>
-              {loading && <div>loading...</div>}
-              {error && <div>{errorMessage}</div>}
-              {!loading && (
-                <button className="submitregister" onClick={handleClick}>
-                  Register
-                </button>
-              )}
             </div>
           </div>
         </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
+    </>
   );
 };
 export default RegisHoster;
